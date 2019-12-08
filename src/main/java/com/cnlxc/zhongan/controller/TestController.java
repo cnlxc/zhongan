8c17b2cbc83d99c4db2485a43821841791d1c1d5
cnlxc <821382992@qq.com>
2019-12-08 23:25:10 +0900

    base security

---
+++ src/main/java/com/cnlxc/zhongan/controller/TestController.java
@@ -0,0 +1,7 @@
+package com.cnlxc.zhongan.controller;
+
+/**
+ * Created by 82138 on 2019/12/8.
+ */
+public class TestController {
+}
---
+++ src/main/java/com/cnlxc/zhongan/payload/SigninRequest.java
@@ -0,0 +1,7 @@
+package com.cnlxc.zhongan.payload;
+
+/**
+ * Created by 82138 on 2019/12/8.
+ */
+public class SigninRequest {
+}
